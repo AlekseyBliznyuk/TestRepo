f533159832484d8950b7c8aa5e9952cf61f8ff35
Aliaksei Blizniuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2025-04-17 14:57:55 +0300

    Add files via upload

---
+++ Lab5TestTask/Services/Implementations/SessionService.cs
@@ -0,0 +1,46 @@
+﻿using Lab5TestTask.Data;
+using Lab5TestTask.Models;
+using Lab5TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5TestTask.Services.Implementations;
+
+/// <summary>
+//SessionService
+/*
+ //возвращает первую сессию рабочего стола
+
+ public async Task<Session> GetSessionAsync()
+{
+return await _dbContext.Sessions.Where(s => s.Type == "Desktop").OrderBy(s => s.StartTime).FirstOrDefaultAsync();
+}
+
+//Возвращает сессии от активных пользователей, завершившихся до 2025 года
+
+public async Task<List<Session>> GetSessionsAsync()
+{
+var currentYear = DateTime.Now.Year;
+return await _dbContext.Sessions.Where(s => s.User.IsActive && s.EndTime < 2025).ToListAsync();
+}
+
+ */
+/// </summary>
+public class SessionService : ISessionService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SessionService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Session> GetSessionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Session>> GetSessionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Lab5TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Lab5TestTask.Data;
+using Lab5TestTask.Models;
+using Lab5TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5TestTask.Services.Implementations;
+
+/// <summary>
+/*
+ //возврат пользователя с наибольшим количеством сессий
+ public async Task<User> GetUserAsync()
+    {
+        return await _dbContext.Users.OrderByDescending(u => u.Sessions.Count).FirstOrDefaultAsync();
+    }
+
+//возврат пользователей, у которых была хоть одна мобильная сессия
+    public async Task<List<User>> GetUsersAsync()
+    {
+        return await _dbContext.Users.Where(u => u.Sessions.Any(s => s.Type == "Mobile")).ToListAsync();
+    }
+ */
+/// </summary>
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<User> GetUserAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<User>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
